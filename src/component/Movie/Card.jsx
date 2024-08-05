0f44e356d64c51aac295d658d0e6b8fd5705c10b
mfauzan8 <mhmmdfauzan296@gmail.com>
2024-08-06 00:33:07 +0700

    add details in card

--- src/component/Movie/Card.jsx
+++ src/component/Movie/Card.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import React from "react";
 import ISO6391 from 'iso-639-1';
 import moment from "moment";
+import { FaStar } from "react-icons/fa";
+import { FaBookmark } from "react-icons/fa6";
+
 
 const Card = ({ movie }) => {
 
@@ -15,6 +18,11 @@ const Card = ({ movie }) => {
           className="cards_img"
           src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
         />
+
+        <div className="flex justify-between">
+          <div className="card_rating flex items-center gap-1"><FaStar className="text-yellow-400" /> {movie.vote_average.toFixed(1)}</div>
+          <div className="card_watchlist"><FaBookmark className="text-red-900" /></div>
+        </div>
         <div className="cards_overlay">
           <div className="cards_title">{movie.original_title}</div>
           <div className="cards_date">{moment(movie.release_date).format('MMM YYYY')} • {ISO6391.getName(movie.original_language)}</div>
--- src/component/Movie/NowPlaying.jsx
+++ src/component/Movie/NowPlaying.jsx
@@ -1,35 +1,60 @@
-import React,{useState, useEffect} from 'react'
-import { getNowPlaying } from '../../api'
-import Card from './Card'
+import React, { useState, useEffect } from 'react';
+import { getNowPlaying } from '../../api';
+import Card from './Card';
 
 const NowPlaying = () => {
-  const [movies, setMovies] = useState([])
-  const [page, setPage] = useState(1)
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-  const fetchData = async()=>{
-    setMovies(await getNowPlaying(page))
-  }
+  const fetchData = async () => {
+    setLoading(true);
+    const data = await getNowPlaying(page);
+    setMovies(data);
+    setLoading(false);
+  };
 
-  useEffect(()=>{fetchData()}, [page])
+  useEffect(() => {
+    fetchData();
+  }, [page]);
 
   return (
-    <section id='now-playing' className='w-screen bg-gray-950 text-white'>
-      <div className='flex justify-between px-10 items-center py-10'>
-      <h2 className='text-3xl font-bold '>Now Playing</h2>
-      <div className='flex gap-3 justify-center items-end'>
-        <button className='bg-red-800 px-2 py-1 rounded-lg disabled:opacity-70 hover:opacity-90' onClick={()=>setPage(page-1)} disabled={page<=1}>Prev</button>
-        <span className='bg-white text-black font-bold px-3 py-1 rounded-lg'>{page}</span>
-        <button className='bg-red-800 px-2 py-1 rounded-lg disabled:opacity-70 hover:opacity-90' onClick={()=>setPage(page+1)}>Next</button>
-      </div>
-      </div>
+    <section id='now-playing' className='w-full bg-gray-950 text-white'>
+      {loading ? (
+        <div className='flex justify-center items-center h-screen'>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <>
+          <div className='flex justify-between px-10 items-center py-10'>
+            <h2 className='text-3xl font-bold'>Now Playing</h2>
+            <div className='flex gap-3 justify-center items-end'>
+              <button
+                className='bg-red-800 px-2 py-1 rounded-lg disabled:opacity-70 hover:opacity-90'
+                onClick={() => setPage(page - 1)}
+                disabled={page <= 1}
+              >
+                Prev
+              </button>
+              <span className='bg-white text-black font-bold px-3 py-1 rounded-lg'>{page}</span>
+              <button
+                className='bg-red-800 px-2 py-1 rounded-lg disabled:opacity-70 hover:opacity-90'
+                onClick={() => setPage(page + 1)}
+              >
+                Next
+              </button>
+            </div>
+          </div>
 
-      <div className='flex flex-wrap justify-center'>
-      {movies?.results?.map(movie=> (
-         <Card movie={movie}/>
-      ))}
-      </div>
+          <div className='flex flex-wrap justify-center'>
+            {movies?.results?.map(movie => (
+              <Card key={movie.id} movie={movie} />
+            ))}
+          </div>
+        </>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default NowPlaying
+export default NowPlaying;
